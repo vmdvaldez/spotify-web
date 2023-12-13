4319d30a0bb232ca228ed18425e185b011456cd2
Vincent Valdez <vmdvaldez@hotmail.com>
2023-12-13 03:39:48 -0500

    add Token Expiration local storage reset

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,26 @@ const CLIENTSECRET= import.meta.env.VITE_CLIENT_SECRET || process.env.VITE_CLIEN
 const REDIRECTURI=import.meta.env.VITE_REDIRECT_URI || process.env.VITE_REDIRECT_URI;
 console.assert(CLIENTID && CLIENTSECRET);
 
+const TOKEN_EXPIRATION = 3600 * 1000; // 3600s -> 3600000ms
+
 function App() {
   const [token, setToken] = useState(null);
 
 
   useEffect( ()=>{
+    let recieved_date = window.localStorage.getItem('token_recieved');
+    if(recieved_date && Date.now() - recieved_date >= TOKEN_EXPIRATION){
+      window.localStorage.clear();
+    }
+
     let storageToken = window.localStorage.getItem('token');
+
 
     if(window.location.hash && !storageToken ){
       storageToken = window.location.hash.substring(1).split('=')[1];
       window.location.hash = "";
       window.localStorage.setItem('token', storageToken);
+      window.localStorage.setItem('token_recieved', Date.now());
     }
     
     if(!storageToken){
